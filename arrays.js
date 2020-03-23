92f6f2f25f52e0f2b521f4d97b7598620231e5f9
puckish-researcher-3224 <kbos22@gmail.com>
2020-03-23 15:41:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ function destructivelyAddElementToBeginningOfArray (arr, elem) {
   return arr;
 }
 function addElementToEndOfArray (arr, elem) {
-   arr.push(elem);
+   arr = [...arr, elem]
   return arr;
 } //always having problems...
 function destructivelyAddElementToEndOfArray (arr, elem) {
